4d618e6e64f8b55cca990a73903606d03ec943a0
Milosz Lewandowski <milosz.lewandowski@icloud.com>
2023-01-10 13:15:55 +0100

    base for RWD

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 'use strict';
 
+const navBar = document.querySelector('.navbar');
 const selectRegion = document.querySelector('select[name="regions"]');
 const searchInput = document.querySelector('#search');
 const flagsBox = document.querySelector('.flags-box');
 const dFlag = document.querySelector('.d-flag');
 const modeButton = document.querySelector('.btn-secondary');
+const flagsContainer = document.querySelector('section');
+const popUp = document.querySelector('.country-container.cont-fired');
 
 const getData = fetch('https://restcountries.com/v3.1/all').then((x) =>
   x.json()
 );
 
-const countryHTML = (country) => `<div class='d-flag'>
+const countryHTML = (country) => `<div class='d-flag' >
   <div class="box-svg"><img class='flag-svg' src='${
     country.flags.svg
   }' alt='' /></div>
@@ -68,4 +71,80 @@ searchInput.addEventListener('input', async function (e) {
 // DARK/LIGHT MODE
 modeButton.addEventListener('click', () => {
   document.body.classList.toggle('dark-mode');
+});
+
+// STICKY NAVBAR
+window.onscroll = () => {
+  if (
+    window.pageYOffset >=
+    searchInput.parentElement.parentElement.offsetTop +
+      searchInput.parentElement.parentElement.offsetHeight
+  ) {
+    // flagsContainer.classList.add('fixed-pos');
+    flagsContainer.style.marginTop = `${navBar.offsetHeight}px`;
+    navBar.classList.add('sticky');
+  } else {
+    navBar.classList.remove('sticky');
+    flagsContainer.style.marginTop = `0`;
+  }
+};
+
+// GET MORE INFO ABOUT COUNTRY by clicking
+flagsBox.addEventListener('click', async function (e) {
+  const getBorders = function (country) {
+    if (country.borders) {
+    }
+    return 'None';
+  };
+
+  const detailsHTML = (country) =>
+    `<div class="container h-100 d-flex align-items-center justify-content-center d-flex">
+    <div class="d-flex align-items-center justify-content-center>
+  <div class="flag-container p-4"><img class='flag-svg' src='${
+    country.flags.svg
+  }' alt='' /></div> <div class="content-container p-4"> <p class="country-name">${
+      country.name.common
+    }</p> <p class="country-population"> <span>Population: </span>${country.population.toLocaleString(
+      'en-US'
+    )}</p> <p class="country-region"><span>Region: </span>${
+      country.region
+    }</p> <p class="country-region"><span>Sub Region: </span>${
+      country.subregion
+    }</p> <p class="country-region"><span>Capital: </span>${
+      country.capital
+    }</p> <p class="country-region"><span>Currencies: </span>${Object.keys(
+      country.currencies
+    )}</p> <p class="country-region"><span>Languages: </span>${Object.values(
+      country.languages
+    )}</p> <p class="country-region"> <span>Border Countries: </span>${getBorders(
+      country
+    )}</p> </div></div> </div> `;
+
+  let dane = await getData;
+  const getCountry = e.target.closest('.d-flag');
+  const getCountryName = getCountry?.children[1].children[0].innerText;
+  const findCountryObj = dane.find(
+    (country) => country.name.common === getCountryName
+  );
+  if (findCountryObj) {
+    flagsBox.nextElementSibling.innerHTML = detailsHTML(findCountryObj);
+    flagsBox.nextElementSibling.classList.remove('hidden');
+    const countryContainer = document.querySelector('.country-container');
+
+    if (navBar.classList.contains('sticky')) {
+      countryContainer.style.transform = `translateY(${
+        window.scrollY - navBar.offsetHeight
+      }px)`;
+    } else {
+      countryContainer.style.transform = `translateY(${window.pageYOffset}px)`;
+    }
+
+    // PREVENT SCROLIING
+    document.body.style.overflow = 'hidden';
+  }
+});
+
+document.querySelector('.cont-fired').addEventListener('click', () => {
+  document.body.style.overflow = 'auto';
+  flagsBox.nextElementSibling.classList.add('hidden');
 });
